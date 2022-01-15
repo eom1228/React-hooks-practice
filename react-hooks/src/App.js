7c4de9e51a7a677a0aae403d387001696486c0bb
Steve Eom <steve@Steveui-iMac.local>
2022-01-16 02:01:07 +0900

    hooks practice-useref&useMemo

---
+++ react-hooks/src/App.js
@@ -0,0 +1,17 @@
+import UseMemoExample from "./components/UseMemoExample";
+import UseRefExample1 from "./components/UseRefExample1";
+import UseRefExample2 from "./components/UseRefExample2";
+import UseRefExample3 from "./components/UseRefExample3";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <UseRefExample1 /> */}
+      {/* <UseRefExample2 /> */}
+      {/* <UseRefExample3 /> */}
+      <UseMemoExample />
+    </div>
+  );
+}
+
+export default App;
